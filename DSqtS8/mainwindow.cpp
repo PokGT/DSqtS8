1a610f9132384e5fb3aaba5b1b6f026f40552a87
PokGT <118605875+PokGT@users.noreply.github.com>
2024-04-12 13:55:14 +0200

    ll

--- DSqtS8/mainwindow.cpp
+++ DSqtS8/mainwindow.cpp
@@ -6,9 +6,23 @@ MainWindow::MainWindow(QWidget *parent)
     , ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
+
+
+    connect(ui->action_ouvrir,&QAction::triggered,this,&MainWindow::ouvrirImage);
+    connect(ui->actionSauvegarder_Image,&QAction::triggered,this,&MainWindow::SauvegarderImage);
 }
 
 MainWindow::~MainWindow()
 {
     delete ui;
+}
+
+void MainWindow::ouvrirImage(void)
+{
+    QString fileName = QFileDialog::getOpenFileName(this, "Sélectionner une image", "", "Images (*.png)");
+    qDebug() << "cc 10";
+}
+void MainWindow::SauvegarderImage(void)
+{
+    qDebug() <<"cc 20";
 }
--- DSqtS8/mainwindow.h
+++ DSqtS8/mainwindow.h
@@ -17,6 +17,9 @@ public:
     MainWindow(QWidget *parent = nullptr);
     ~MainWindow();
 
+    void ouvrirImage(void);
+    void SauvegarderImage(void);
+
 private:
     Ui::MainWindow *ui;
 };
